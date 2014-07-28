d3e2f521dbcfb221334ed12989675717f476546c
Chyld Medford <chyld.medford@gmail.com>
2014-07-28 17:56:30 -0500

    done

---
+++ app/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/static'));
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+app.get('/about', function(req, res){
+  res.render('about');
+});
+
+app.get('/discography', function(req, res){
+  res.render('discography');
+});
+
+app.get('/tours', function(req, res){
+  res.render('tours');
+});
+
+app.listen(4000, function(){
+  console.log('Fall Out Boy is playing!');
+});
+
